dbcde32022fa49b375c31277f75d5accdff58a3c
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2015-01-29 23:32:22 +0100

    Avoid traversal of parent directory with ADD in Dockerfiles
    This duplication makes me really sad, but is a restriction imposed
    by Docker.

---
+++ stable/entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+ARTIFACTS=/tmp/artifacts
+DISPLAY=:1
+SCREEN=0
+RESOLUTION=1920x1080
+
+mkdir -p $ARTIFACTS
+
+function wrap() {
+  $@ > $ARTIFACTS/$1.out 2> $ARTIFACTS/$1.err &
+}
+
+wrap Xvfb \
+  $DISPLAY \
+  -screen $SCREEN \
+  ${RESOLUTION}x24
+
+sleep 1
+
+wrap x11vnc \
+  -geometry $RESOLUTION \
+  -display $DISPLAY \
+  -shared \
+  -forever \
+  -passwd stsylatac
+
+wrap avconv \
+  -f x11grab \
+  -s $RESOLUTION \
+  -i $DISPLAY.$SCREEN+nomouse \
+  -r 20 \
+  -vcodec libvpx \
+  $ARTIFACTS/screencast.webm
+
+wrap tcpdump \
+  -w $ARTIFACTS/tcpdump.pcap
+
+DISPLAY=$DISPLAY wrap chromedriver \
+  --hostname=127.0.0.1 \
+  --port=4444 \
+  --url-base=/wd/hub \
+  --whitelisted-ips \
+  --verbose
+
+while [ true ] ; do
+  sleep 60
+  echo heartbeat
+done
